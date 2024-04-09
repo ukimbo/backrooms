d316c5f06a4be2f27063464aad3b2282f47ea0e4
Menimobong Utibe-Ukim <menimobong@berkeley.edu>
2024-04-09 01:13:17 -0700

    Initalized World.java with our starting classes and instance variables

--- proj3/src/core/World.java
+++ proj3/src/core/World.java
@@ -1,7 +1,40 @@
 package core;
 
-public class World {
+import tileengine.TETile;
 
+import java.util.List;
+
+public class World {
     // build your own world!
+    private TETile[][] worldArray;
+    private final String SEED;
+    private int height;
+    private int width;
+    private int totalArea;
+    private int usedArea;
+    private List<Room> Rooms;
+
+    public World(String seed) {
+        SEED = seed;
+    }
+
+    private class Room {
+        private int[] bottomLeftCords;
+        private int[] topLeftCords;
+        private int length;
+        private int height;
+        private int area;
+        int numberOfSockets;
+        int[] socketCords; //We can adjust this to apply for when numberOfSockets > 1
+
+
+    }
+
+    private class Hallway extends Room {
+        int[] startCords;
+        int[] endCords;
+        boolean isVertical;
+        Room[] bridgeBetween;
+    }
 
 }
