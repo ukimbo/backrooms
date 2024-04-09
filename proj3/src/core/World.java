fe2ce10994afe45e38154bbb0b6cafba37f912c0
Menimobong Utibe-Ukim <menimobong@berkeley.edu>
2024-04-09 16:27:28 -0700

    Ezra on men comp

--- proj3/src/core/World.java
+++ proj3/src/core/World.java
@@ -1,6 +1,8 @@
 package core;
 
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 import tileengine.TETile;
+import tileengine.Tileset;
 
 import java.util.List;
 
@@ -8,14 +10,26 @@ public class World {
     // build your own world!
     private TETile[][] worldArray;
     private final String SEED;
-    private int height;
-    private int width;
+    private static final int screenWidth = 80;
+    private static final int screenHeight = 45;
+
     private int totalArea;
     private int usedArea;
     private List<Room> Rooms;
+    private WeightedQuickUnionUF roomConnections;
 
     public World(String seed) {
         SEED = seed;
+        worldArray = new TETile[screenWidth][screenHeight];
+        for (int x = 0; x < screenWidth; x++) {
+            for (int y = 0; y < screenHeight; y++) {
+                worldArray[x][y] = Tileset.NOTHING;
+            }
+        }
+    }
+
+    public void drawRoom(Room room) {
+
     }
 
     private class Room {
@@ -24,6 +38,7 @@ public class World {
         private int length;
         private int height;
         private int area;
+        private int id;
         int numberOfSockets;
         int[] socketCords; //We can adjust this to apply for when numberOfSockets > 1
 
