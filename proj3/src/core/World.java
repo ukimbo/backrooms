0918a6ff78532900fa117891e03335eef185cee4
Menimobong Utibe-Ukim <menimobong@berkeley.edu>
2024-04-09 18:59:22 -0700

    Implemented randomHalls in order to randomly generate hallways that connect to rooms, haven't made halls accesible tho

--- proj3/src/core/World.java
+++ proj3/src/core/World.java
@@ -14,7 +14,7 @@ public class World {
     private TETile[][] worldArray;
 
     private int totalArea;
-    private int usedArea;
+//    private int usedArea;
     private List<Room> Rooms;
 //    private WeightedQuickUnionUF roomConnections;
     public TETile[][] world;
@@ -28,7 +28,7 @@ public class World {
         this.width = width;
         this.height = height;
         this.totalArea = this.width * this.height;
-        this.usedArea = 0;
+//        this.usedArea = 0;
         this.Rooms = new ArrayList<>();
         this.world = new TETile[width][height];
         System.out.println(seed);
@@ -40,12 +40,14 @@ public class World {
             }
         }
         this.randomSquare();
+        this.randomHalls();
     }
 
     public void randomSquare() {
         boolean placed = false;
         int count = 0;
-        int roomNumber = RandomUtils.uniform(random, 19, 25);
+        //int roomNumber = RandomUtils.uniform(random, 19, 25);
+        int roomNumber = 25;
         while (!placed) {
             int floorWidth = RandomUtils.uniform(random, 2, 15);
             int floorHeight = RandomUtils.uniform(random, 2, 15);
@@ -53,11 +55,52 @@ public class World {
             int yStart = RandomUtils.uniform(random, 1, height - floorHeight - 1);
 
             if (!overlaps(xStart, yStart, floorWidth, floorHeight)) {
-                drawRoom(xStart, yStart, floorWidth, floorHeight);
+                drawRoom(xStart, yStart, floorWidth, floorHeight, false);
                 if (count == roomNumber) {
                     placed = true;
                 }
                 count += 1;
+            }
+
+        }
+    }
+
+    public void randomHalls() {
+        boolean placed = false;
+        int count = 0;
+        int roomNumber = RandomUtils.uniform(random, 19, 25);
+        while (!placed) {
+            int floorLength = RandomUtils.uniform(random, 5, 15);
+            int hallwayHeight = 1;
+            boolean isVertical = RandomUtils.bernoulli(random);
+            if (isVertical) {
+                int xStart = RandomUtils.uniform(random, 1, width - 1);
+                int yStart = RandomUtils.uniform(random, 1, height - floorLength - 1);
+                //Checks if it overlaps on anything besides the ends
+                if (!overlaps(xStart, yStart + 1, hallwayHeight, floorLength - 2)) {
+                    //checks if specifically the ends of the hallways overlap
+                    if (endsOverlaps(xStart, yStart, floorLength, isVertical)) {
+                        drawRoom(xStart, yStart, hallwayHeight, floorLength,true);
+                        if (count == roomNumber) {
+                            placed = true;
+                        }
+                        count += 1;
+                    }
+                }
+            } else {
+                int xStart = RandomUtils.uniform(random, 1, width - floorLength - 1);
+                int yStart = RandomUtils.uniform(random, 1, height - 1);
+                //Checks if it overlaps on anything besides the ends
+                if (!overlaps(xStart + 1, yStart, floorLength - 2, hallwayHeight)) {
+                    //checks if specifically the ends of the hallways overlap
+                    if (endsOverlaps(xStart, yStart, floorLength, isVertical)) {
+                        drawRoom(xStart, yStart, floorLength, hallwayHeight,true);
+                        if (count == roomNumber) {
+                            placed = true;
+                        }
+                        count += 1;
+                    }
+                }
             }
 
         }
@@ -74,12 +117,38 @@ public class World {
         }
         return false;
     }
+    //if ends both ends overlap but not the rest of the room
+    private boolean endsOverlaps(int xStart, int yStart, int floorLength, boolean isVertical) {
+        if (isVertical) {
+            if (//!world[xStart - 1][yStart - 1].equals(Tileset.NOTHING) &&
+                    !world[xStart][yStart - 1].equals(Tileset.NOTHING) &&
+                    //!world[xStart + 1][yStart - 1].equals(Tileset.NOTHING) &&
+                    //!world[xStart - 1][yStart + floorLength].equals(Tileset.NOTHING) &&
+                    !world[xStart][yStart + floorLength].equals(Tileset.NOTHING)// &&
+                    /*!world[xStart + 1][yStart + floorLength].equals(Tileset.NOTHING)*/) {
+                return true;
+            }
+        } else {
+            if (//!world[xStart - 1][yStart + 1].equals(Tileset.NOTHING) &&
+                    !world[xStart - 1][yStart].equals(Tileset.NOTHING) &&
+                    //!world[xStart - 1][yStart - 1].equals(Tileset.NOTHING) &&
+                    //!world[xStart + floorLength][yStart + 1].equals(Tileset.NOTHING) &&
+                    !world[xStart + floorLength][yStart].equals(Tileset.NOTHING)// &&
+                    /*!world[xStart + floorLength][yStart - 1].equals(Tileset.NOTHING)*/) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 
-    private void drawRoom(int xStart, int yStart, int floorWidth, int floorHeight) {
+    private void drawRoom(int xStart, int yStart, int floorWidth, int floorHeight, boolean isHallway) {
         //Keeping track of Rooms
-        Room room = new Room(xStart, yStart, floorWidth, floorHeight);
-        Rooms.add(room);
+        if (!isHallway) {
+            Room room = new Room(xStart, yStart, floorWidth, floorHeight);
+            Rooms.add(room);
+        }
+
         //floors
         for (int x = xStart; x < xStart + floorWidth; x++) {
             for (int y = yStart; y < yStart + floorHeight; y++) {
@@ -102,7 +171,6 @@ public class World {
         private int length;
         private int height;
         private int area;
-        private int id;
         public Room(int xStart, int yStart, int floorWidth, int floorHeight) {
             bottomLeftCords = new int[2];
             topLeftCords = new int[2];
@@ -113,20 +181,17 @@ public class World {
             length = floorWidth + 1;
             height = floorHeight + 1;
             area = length * width;
-            usedArea += area;
+//            usedArea += area;
         }
 
 
     }
 
     private class Hallway extends Room {
-        int[] startCords;
-        int[] endCords;
         boolean isVertical;
-        Room[] bridgeBetween;
 
-        public Hallway(int xStart, int yStart, int floorWidth, int floorHeight) {
-            super(xStart, yStart, floorWidth, floorHeight);
+        public Hallway(int xStart, int yStart, int length, boolean isVertical) {
+            super(xStart, yStart, (isVertical ? 1: length), (isVertical ? length: 1));
         }
     }
 
